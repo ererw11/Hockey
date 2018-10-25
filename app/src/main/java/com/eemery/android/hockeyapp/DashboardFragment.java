babd0d2d85bfc2fea2025ee439436e30094aa934
Eric Emery <eric.r.emery@gmail.com>
2018-10-24 19:27:27 -0500

    feat: Started adding RecyclerView of teams on Dashboard

--- app/src/main/java/com/eemery/android/hockeyapp/DashboardFragment.java
+++ app/src/main/java/com/eemery/android/hockeyapp/DashboardFragment.java
@@ -1,25 +1,55 @@
 package com.eemery.android.hockeyapp;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
-public class DashboardFragment extends Fragment {
+import com.eemery.android.hockeyapp.team.Team_;
+
+public class DashboardFragment extends Fragment implements TeamAdapter.TeamAdapterOnClickHandler {
+
+    private static final String TAG = DashboardFragment.class.getSimpleName();
+
+    private RecyclerView teamsRecyclerView;
 
 
     public DashboardFragment() {
         // Required empty public constructor
     }
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRetainInstance(true);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dashboard, container, false);
+        View v = inflater.inflate(R.layout.fragment_dashboard, container, false);
+
+        teamsRecyclerView = v.findViewById(R.id.teams_recycler_view);
+
+        RecyclerView.LayoutManager teamsLayoutManager =
+                new LinearLayoutManager(getContext(), RecyclerView.HORIZONTAL, false);
+        teamsRecyclerView.setLayoutManager(teamsLayoutManager);
+        teamsRecyclerView.setHasFixedSize(true);
+
+        return v;
     }
 
+
+    @Override
+    public void onClick(Team_ team) {
+        // Create a toast to test the onClick
+        Toast.makeText(getContext(), team.getTeamName(), Toast.LENGTH_SHORT).show();
+    }
 }
---
+++ app/src/main/java/com/eemery/android/hockeyapp/TeamAdapter.java
@@ -0,0 +1,78 @@
+package com.eemery.android.hockeyapp;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.eemery.android.hockeyapp.team.Team_;
+
+import java.util.List;
+
+public class TeamAdapter extends RecyclerView.Adapter<TeamAdapter.TeamHolder> {
+
+    private final TeamAdapterOnClickHandler mClickHandler;
+    private final List<Team_> teamList;
+
+    public TeamAdapter(List<Team_> teams, TeamAdapterOnClickHandler clickHandler) {
+        teamList = teams;
+        mClickHandler = clickHandler;
+    }
+
+    @Override
+    public TeamAdapter.TeamHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View view = inflater.inflate(R.layout.item_view_dashboard_teams, parent, false);
+        return new TeamAdapter.TeamHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(TeamAdapter.TeamHolder holder, int position) {
+        Team_ team = teamList.get(position);
+        String teamLocation = team.getLocationName();
+        String teamName = team.getTeamName();
+        String teamDivision = team.getDivision().getName();
+        holder.bindView(teamLocation, teamName, teamDivision);
+    }
+
+    @Override
+    public int getItemCount() {
+        if (teamList == null) {
+            return 0;
+        }
+        return teamList.size();
+    }
+
+    public interface TeamAdapterOnClickHandler {
+        void onClick(Team_ team);
+    }
+
+    public class TeamHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        private final TextView teamCityTextView;
+        private final TextView teamNameTextView;
+        private final TextView teamConferenceTextView;
+
+        TeamHolder(View itemView) {
+            super(itemView);
+
+            teamCityTextView = itemView.findViewById(R.id.team_city);
+            teamNameTextView = itemView.findViewById(R.id.team_name);
+            teamConferenceTextView = itemView.findViewById(R.id.team_division);
+            itemView.setOnClickListener(this);
+        }
+
+        void bindView(String teamLocation, String teamName, String conference) {
+            teamCityTextView.setText(teamLocation);
+            teamNameTextView.setText(teamName);
+            teamConferenceTextView.setText(conference);
+        }
+
+        @Override
+        public void onClick(View v) {
+            int adapterPosition = getAdapterPosition();
+            Team_ selectedTeam = teamList.get(adapterPosition);
+            mClickHandler.onClick(selectedTeam);
+        }
+    }
+}
